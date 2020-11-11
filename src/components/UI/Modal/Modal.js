0b4466a05ca5757095af99b7af46d0ae1c0381fd
divy sen <divysen.astro@gmail.com>
2020-11-11 16:43:21 +0530

    8. A Real App The Burger Builder (Basic Version) part 3

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
+// import { Component } from 'react';
 import Auxiliary from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 import StyleClass from './Modal.css';
 
-const Modal = props => (
-    <Auxiliary>
-        <Backdrop show={props.show} back={props.closeModal}/>
-        <div className={StyleClass.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>  
-    </Auxiliary>
-);
+const Modal = props => {
+
+    useEffect( () => {
+        console.log(`[Modal.js] useEffect updated with props.show`)
+    }, [props.show]);
+
+    return(
+        <Auxiliary>
+            <Backdrop show={props.show} back={props.closeModal}/>
+            <div className={StyleClass.Modal}
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
+                {props.children}
+            </div>  
+        </Auxiliary>
+    );
+}
+
+// class Modal extends Component{
+
+//     shouldComponentUpdate(nextProps, nextState){
+//         if(nextProps.show !== this.props.show){
+//             console.log(`[Modal.js updated]`);
+//             return true;
+//         }
+//         else{
+//             return false;
+//         } 
+//     }
+
+//     render(){
+//         return(
+//             <Auxiliary>
+//                 <Backdrop show={this.props.show} back={this.props.closeModal}/>
+//                 <div className={StyleClass.Modal}
+//                     style={{
+//                         transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+//                         opacity: this.props.show ? '1' : '0'
+//                     }}>
+//                     {this.props.children}
+//                 </div>  
+//             </Auxiliary>
+//         );
+//     };
+// }
 
 export default Modal;
